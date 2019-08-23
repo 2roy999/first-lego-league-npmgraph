ed27823d703527e30870435f1b4947afaf26ea74
2roy999 <2903801+2roy999@users.noreply.github.com>
2019-08-23 17:57:34 +0300

    Update scope.js

--- js/scope.js
+++ js/scope.js
@@ -5,12 +5,12 @@ export function isInScope(m) {
 }
 
 export const startingModules = [
-  '@first-lego-league/clock@2.4.2',
-  '@first-lego-league/display@2.4.3',
+  '@first-lego-league/clock@2.5.0',
+  '@first-lego-league/display@2.6.0',
   // '@first-lego-league/display-system',
   // '@first-lego-league/eslint-config',
   // '@first-lego-league/fllscoring',
-  '@first-lego-league/image-server',
+  '@first-lego-league/identity-provider@1.1.1',
   // '@first-lego-league/ms-auth',
   // '@first-lego-league/ms-authentication',
   // '@first-lego-league/ms-client',
@@ -19,8 +19,9 @@ export const startingModules = [
   // '@first-lego-league/ms-logger',
   // '@first-lego-league/ms-messenger',
   // '@first-lego-league/protocols',
-  '@first-lego-league/scoring@2.3.8',
-  '@first-lego-league/tournament@2.0.2',
-  '@first-lego-league/tournament-status@1.0.1',
+  '@first-lego-league/rankings@1.0.5',
+  '@first-lego-league/scoring@2.4.0',
+  '@first-lego-league/tournament@2.1.0',
+  '@first-lego-league/tournament-status@1.3.0',
   // '@first-lego-league/user-interface'
 ];
